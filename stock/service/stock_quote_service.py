2636162d56701298cd6a226b9f298c2f6008468a
donghyun.suh <adamsuh008@gmail.com>
2026-04-20 23:46:37 +0900

    feat: 주식 정보 조회 서비스 추가
    - API Client를 주입받아 주식 정보 조회 로직 구성
    - 도메인(Stock) 반환하는 서비스 계층 구현

---
+++ stock/service/stock_quote_service.py
@@ -0,0 +1,9 @@
+from stock.domain.adapter.api_client import IApiClient
+
+
+class StockQuoteService:
+    def __init__(self, api_client: IApiClient):
+        self.api_client = api_client
+
+    def get_stock_info(self, market: str, code: str):
+        return self.api_client.get_stock_info(market, code)
